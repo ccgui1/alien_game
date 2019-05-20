0e30b1190dbd466e7951c23cb3dd4f09faca69c2
ccgui1 <1627389570@qq.com>
2019-05-20 18:52:42 +0800

    settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """存储 外星人 的所有设置的类 """
+
+    def __init__(self):
+        """初始化游戏的设置"""
+
+        # 屏幕设置
+        self.screen_width = 640
+        self.screen_height = 480
+        self.bg_color = (230, 230, 230)
